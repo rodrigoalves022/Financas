c76bb400596c4544f3f3e795603b2b76cfe7f7b0
rodrigoalves022 <88457271+rodrigoalves022@users.noreply.github.com>
2026-05-02 15:03:14 -0300

    Adiciona build Android com Capacitor

---
+++ finance-app-react/android/app/src/main/java/br/com/rodrigo/financas/MainActivity.java
@@ -0,0 +1,5 @@
+package br.com.rodrigo.financas;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ finance-app-react/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.rodrigo.financas',
+  appName: 'Controle Financeiro',
+  webDir: 'dist'
+};
+
+export default config;
